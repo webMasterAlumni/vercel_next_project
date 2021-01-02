09fe20d30c9ba6b805ed8f60977db29ad91c307f
webMasterAlumni <69198104+webMasterAlumni@users.noreply.github.com>
2021-01-02 17:34:30 -0300

    primeira otimização de rotas

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
-import { useState } from 'react'
+import Link from 'next/link'
 
 function Home() {
     return (
         <div>
             <div>Home</div>
-            <Contador />
+            <Link href="/sobre">
+                <a >Acessar página Sobre</a>
+            </Link>
         </div>
     )
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1)
-
-    function adicionarContador() {
-        setContador(contador + 1)
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Sobre() {
+    return (
+        <div>
+            <div>Sobre</div>
+            <Link href="/">
+                <a >Acessar página Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
